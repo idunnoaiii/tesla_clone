38978efdd708641446b80ece39ef83df5e317a9c
Thien Nguyen <thiennq62515@gmail.com>
2021-10-09 12:16:20 +0700

    ADD: sections

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "components/Section";
+
+function Home() {
+  return (
+    <Container>
+      <Section></Section>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+export default Home;
---
+++ src/components/Section.js
@@ -0,0 +1,100 @@
+import { textColor } from "common/theme";
+import React from "react";
+import styled from "styled-components";
+
+function Section() {
+  return (
+    <Wrap>
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom Order</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg"></DownArrow>
+      </Buttons>
+    </Wrap>
+  );
+}
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url("/images/model-3.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const LeftButton = styled.button`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  outline: none;
+  border: none;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+
+  ${LeftButton} {
+    margin: 8px;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  color: ${textColor};
+  opacity: 0.65;
+`;
+
+const Buttons = styled.div``;
+
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+
+  @keyframes animateDown {
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+      transform: translateY(0);
+    }
+    40% {
+      transform: translateY(5px) rotateZ(10deg);
+    }
+    60% {
+      transform: translateY(3px) rotateZ(-10deg);
+    }
+  }
+`;
+
+export default Section;
